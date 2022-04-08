5b4fd5ff7071ac1fc04caee4464d7eabc992b0a9
mohammed <101970178+gittest55@users.noreply.github.com>
2022-04-08 16:12:43 +0300

    Update py-pass.py

--- python-pass/py-pass.py
+++ python-pass/py-pass.py
@@ -2,18 +2,16 @@ def num_check ():
     
 
  x=int(input("Enter X Value: "))
- if (x<1 or x>10):
-         print("try another  number ")
- else:    
-
-  b=[]
     
-  for i in range(int(x)):
+
+ b=[]
+    
+ for i in range(int(x)):
     c=int(input())
     b.insert(i,c)
 
 
-  for i in range(x):
+ for i in range(x):
     if (b[i]%2==0):
         print(str(b[i])+" is even")
     else:
