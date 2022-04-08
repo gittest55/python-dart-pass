6a13e912c4bc8886bf0a72f586377f6363e2e726
mohammed <101970178+gittest55@users.noreply.github.com>
2022-04-08 18:14:36 +0300

    Update and rename test.md to const_pro.dart

---
+++ dart-pass/const_pro.dart
@@ -0,0 +1,29 @@
+
+main (){
+  
+  int hour= DateTime.now().hour;
+  
+  // now() is consided a Constructor and the data is being changed Approximately every mille secounds
+  /* Const variables must be initialized with a constant value.
+   Example:
+   */
+  const a=5;
+  const b=a;
+  //True
+  
+  int   q=5;
+  const f=a;
+  //False
+  
+  //Since q is not const and can be changed any time 
+
+
+
+  
+  /* Solution
+  
+    var _hour= DateTime.now().hour;
+
+  */
+  
+}
